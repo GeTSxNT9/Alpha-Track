3a7ecac828fb8343e53de798be599195c384d662
GeTSNT9 <cwgetsxnt9@gmail.com>
2026-01-08 00:46:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
 const CACHE_NAME = 'alpha-v_final_scroll';
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './manifest.json']))
+  );
+  self.skipWaiting(); // <--- Esto obliga a la nueva versión a activarse de inmediato
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
