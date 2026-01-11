c4e143d579933a77ad9c701cef811bf31275a760
GeTSNT9 <cwgetsxnt9@gmail.com>
2026-01-11 15:41:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = 'alpha-v5';
+const CACHE_NAME = 'alpha-v5.1';
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll([
       './', 
       './index.html', 
-      './manifest.json'
+      './manifest.json',
+      'https://api.dicebear.com/7.x/initials/svg?seed=A&backgroundColor=000000&fontSize=60&fontWeight=700'
     ]))
   );
   self.skipWaiting();
